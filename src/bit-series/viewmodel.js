1a5bc7e5b817f3f6ddc2ef991cad09352520723e
Kyle Gifford <kyle@kylegifford.com>
2015-05-14 10:07:36 -0500

    adds doc blocks to bit-series

--- src/bit-series/viewmodel.js
+++ src/bit-series/viewmodel.js
@@ -4,29 +4,67 @@ import d3 from "d3";
 
 export default can.Map.extend({
     define: {
+        /**
+         * @property {can.List} series.viewModel.serializedData serializedData
+         * @parent series/viewModel
+         * @description Serialized version of data. This is populated automatically when data changes.
+         * @option {can.List} Defaults to `undefined`.
+         */
         serializedData: {
             get: function() {
                 return this.attr('data') && this.attr('data').serialize() || null;
             },
         },
+
+        /**
+         * @property {Number} series.viewModel.seriesLength seriesLength
+         * @parent series/viewModel
+         * @description Length of the series' data.
+         * @option {Number} Defaults to `undefined`.
+         */
         seriesLength: {
+            // TODO should this have a default value of 0?
             get: function() {
                 return this.attr('data') && this.attr('data').attr('length') || 0;
             }
         },
+
+        /**
+         * @property {Number} series.viewModel.lowestValue lowestValue
+         * @parent series/viewModel
+         * @description Lowest value of the series' data.
+         * @option {Number} Defaults to `undefined`.
+         */
         lowestValue: {
+            // TODO should this default to 0?
             get: function() {
                 var lowestValue = this.attr('serializedData') && d3.min(this.attr('serializedData'));
                 return (lowestValue || lowestValue === 0) ? lowestValue : null;
             }
         },
+
+        /**
+         * @property {Number} series.viewModel.highestValue highestValue
+         * @parent series/viewModel
+         * @description Hightest value of the series' data.
+         * @option {Number} Defaults to `undefined`.
+         */
         highestValue: {
+            // TODO should this default to 0?
             get: function() {
                 var highestValue = this.attr('serializedData') && d3.max(this.attr('serializedData'));
                 return (highestValue || highestValue === 0) ? highestValue : null;
             }
         },
+
+        /**
+         * @property {Object} series.viewModel.line line
+         * @parent series/viewModel
+         * @description D3 line object representing the series' data, scaled using the parent component (`bit-graph`)'s X and Y scales.
+         * @option {Number} Defaults to `undefined`.
+         */
         line: {
+            // TODO should this have a default value of null?
             get: function() {
                 if(this.attr('graph') && this.attr('seriesLength') && this.attr('lowestValue') !== null && this.attr('highestValue') !== null) {
                     if(this.attr('graph').attr('normalize')) {
@@ -62,18 +100,45 @@ export default can.Map.extend({
                 }
             }
         },
+
+        /**
+         * @property {can.List} series.viewModel.data data
+         * @parent series/viewModel
+         * @description Raw series' data. All values are converted to Numbers.
+         * @option {can.List} Defaults to `undefined`.
+         */
         data: {
             get: function(value) {
                 return this.convertListItemsToNumbers(value);
             }
         }
     },
+
+    /**
+     * @property {String} series.viewModel.color color
+     * @parent series/viewModel
+     * @description Color of the series' line. Can be a hex value or a supported color name.
+     * @option {String} Defaults to `steelblue`.
+     */
     color: "steelblue",
-    data: null,
+
+    /**
+     * @property {BitGraphVM} series.viewModel.graph graph
+     * @parent series/viewModel
+     * @description Parent container object (`bit-graph`) of the series. Set automatically when inserted.
+     * @option {BitGraphVM} Defaults to `null`.
+     */
     graph: null,
+
+    /**
+     * @function series.viewModel.convertListItemsToNumbers convertListItemsToNumbers
+     * @parent series/viewModel
+     * @description Converts list items to float values. Removes NaN values.
+     * @param {can.List} listItems List who's contents are to be converted to numbers.
+     */
     convertListItemsToNumbers: function(listItems) {
         if(listItems) {
-                listItems.each(function(data, index) {
+            listItems.each(function(data, index) {
                 var floatValue = Number.parseFloat(data);
                 if(!Number.isNaN(floatValue)) {
                     listItems[index] = floatValue;
