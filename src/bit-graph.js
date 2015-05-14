a2622796caee0167dc9105f90b7c749b6a7f847e
Kyle Gifford <kyle@kylegifford.com>
2015-05-14 09:39:05 -0500

    bit-graph doc blocks added

--- src/bit-graph.js
+++ src/bit-graph.js
@@ -1,9 +1,49 @@
+/**
+ * @module {can.Component} graph Graph
+ * @parent components
+ * @group graph/events 0 Events
+ * @group graph/viewModel 1 ViewModel
+ *
+ * @author Kyle Gifford
+ *
+ *
+ * @description
+ * The graph component renders a live-updating line graph of multiple sets of data.
+ *
+ *
+ * @signature '<bit-graph></bit-graph>'
+ *
+ * @param {Array} margins Margins of the graph inside the SVG element.
+ * @param {Number} height The height of the graph element.
+ * @param {Number} width The width of the graph element.
+ * @param {boolean} normalize Whether to normalize series data against each other or not.
+ *
+ * @body
+ *
+ * ## Component Initialization
+ *
+ * ```html
+ *   <bit-graph></bit-graph>
+ * ```
+ *
+ * The layout of the graph can be modified with the `width`, `height`, and `margins` parameters.
+ * ```html
+ *   <bit-graph width="500" height="300"></bit-graph>
+ * ```
+ *
+ * The graph can be set to normalize mode by adding the `normalize` boolean parameter
+ * ```html
+ *   <bit-graph normalize></bit-graph>
+ * ```
+ *
+ * @demo bit-graph.html
+ */
 import can from "can";
 import 'can/map/define/';
+import d3 from "d3";
 import './bit-series/'
 import template from "./bit-graph.stache!";
 import './bit-graph.less!';
-import d3 from "d3";
 import BitGraphVM from './viewmodel';
 
 can.Component.extend({
@@ -11,21 +51,29 @@ can.Component.extend({
     template: template,
     viewModel: BitGraphVM,
     events: {
-        inserted: function(scope, ev) {
+
+        /**
+         * @function graph.events.inserted Graph Inserted Event
+         * @parent autocomplete/events
+         * @description Sets up the graph container elements in D3 and builds the base graph SVG elements.
+         * @param {viewModel} viewModel The viewModel of the graph
+         * @param {event} ev The jQuery event triggered by DOM insertion 
+         */
+        inserted: function(viewModel, ev) {
             var rootElement = ev.target,
                 graphBaseElement = d3.select(rootElement.getElementsByClassName('graph')[0]);
-            this.scope.renderBaseGraph(graphBaseElement);
+            this.viewModel.renderBaseGraph(graphBaseElement);
         },
-        // "{scope} xScale": function() {
-        //     this.scope.refreshAxes();
-        // },
-        // "{scope} yScale": function() {
-        //     this.scope.refreshAxes();
-        // },
-        "{scope} seriesSerialized": function() {
+
+        /**
+         * @function graph.events.seriesSerialized Serialized Series Change Event
+         * @parent autocomplete/events
+         * @description Refresh graph data and axes when series' line(s) change.
+         */
+        "{viewModel} seriesSerialized": function() {
             // TODO figure out how these can just listen and auto refresh
-            this.scope.refreshAxes();
-            this.scope.refreshGraph();
+            this.viewModel.refreshAxes();
+            this.viewModel.refreshGraph();
         }
     }
 });
--- src/viewmodel.js
+++ src/viewmodel.js
@@ -4,10 +4,24 @@ import d3 from "d3";
 
 export default can.Map.extend({
     define: {
+
+        /**
+         * @property {Array} graph.viewModel.margins margins
+         * @parent graph/viewModel
+         * @description Margins between the graph and its containing element. Should be a four-length array similar to a CSS margin property (`[margin-top, margin-right, margin-bottom, margin-left]`)
+         * @option {Array} Defaults to `[20, 0, 20, 80]`.
+         */
         margins: {
             value: [20, 0, 20, 80],
             type: "*"
         },
+
+        /**
+         * @property {Number} graph.viewModel.width width
+         * @parent graph/viewModel
+         * @description Width, in pixels, of the graph container. Note: the actual graph may be smaller due to margins, axes, etc.
+         * @option {Number} Defaults to `768`.
+         */
         width: {
             value: 768,
             get: function(val) {
@@ -15,6 +29,13 @@ export default can.Map.extend({
                 return val - margins[1] - margins[3];
             }
         },
+
+        /**
+         * @property {Number} graph.viewModel.height height
+         * @parent graph/viewModel
+         * @description Height, in pixels, of the graph container. Note: the actual graph may be smaller due to margins, axes, etc.
+         * @option {Number} Defaults to `400`.
+         */
         height: {
             value: 400,
             get: function(val) {
@@ -22,30 +43,79 @@ export default can.Map.extend({
                 return val - margins[0] - margins[2];
             }
         },
+
+        /**
+         * @property {String} graph.viewModel.transform transform
+         * @parent graph/viewModel
+         * @description Transformation function to be added to the graph container's grouping element.
+         * @option {String} Defaults to `translate(80, 20)`.
+         */
         transform: {
             get: function() {
                 var margins = this.attr('margins');
                 return "translate(" + margins[3] + "," + margins[0] + ")"
             }
         },
+
+        /**
+         * @property {Object} graph.viewModel.graphContainerElement graphContainerElement
+         * @parent graph/viewModel
+         * @description D3 element that contains all graph elements. This is set automatically when the component is inserted.
+         * @option {Object} Defaults to `undefined`.
+         */
         graphContainerElement: {
             type: "*"
         },
+
+        /**
+         * @property {Object} graph.viewModel.lineContainerElement lineContainerElement
+         * @parent graph/viewModel
+         * @description D3 element that contains all series' line elements. This is set automatically when the component is inserted.
+         * @option {Object} Defaults to `undefined`.
+         */
         lineContainerElement: {
             type: "*"
         },
+
+        /**
+         * @property {Object} graph.viewModel.axisContainerElement axisContainerElement
+         * @parent graph/viewModel
+         * @description D3 element that contains all axis elements. This is set automatically when the component is inserted.
+         * @option {Object} Defaults to `undefined`.
+         */
         axisContainerElement: {
             type: "*"
         },
+
+        /**
+         * @property {Boolean} graph.viewModel.normalize normalize
+         * @parent graph/viewModel
+         * @description Flag to indicate whether to normalize all series' data.
+         * @option {Boolean} Defaults to `false`.
+         */
         normalize: {
             value: false,
             type: "htmlbool"
         },
+
+        /**
+         * @property {can.Map} graph.viewModel.seriesSerialized seriesSerialized
+         * @parent graph/viewModel
+         * @description Serialized version of series data. This is populated automatically when series are added to the graph using the `bit-series` sub-component.
+         * @option {can.Map} Defaults to `undefined`.
+         */
         seriesSerialized: {
             get: function() {
                 return this.attr('series').serialize();
             }
         },
+
+        /**
+         * @property {Number} graph.viewModel.longestSeriesLength longestSeriesLength
+         * @parent graph/viewModel
+         * @description Longest length of any series added to the graph using the `bit-series` subcomponent.
+         * @option {Number} Defaults to `0`.
+         */
         longestSeriesLength: {
             value: 0,
             get: function() {
@@ -59,6 +129,13 @@ export default can.Map.extend({
                 return 0;
             }
         },
+
+        /**
+         * @property {Number} graph.viewModel.lowestSeriesValue lowestSeriesValue
+         * @parent graph/viewModel
+         * @description Lowest value of any series added to the graph using the `bit-series` subcomponent.
+         * @option {Number} Defaults to `0`.
+         */
         lowestSeriesValue: {
             value: 0,
             get: function() {
@@ -72,6 +149,13 @@ export default can.Map.extend({
                 return 0;
             }
         },
+
+        /**
+         * @property {Number} graph.viewModel.highestSeriesValue highestSeriesValue
+         * @parent graph/viewModel
+         * @description Highest value of any series added to the graph using the `bit-series` subcomponent.
+         * @option {Number} Defaults to `0`.
+         */
         highestSeriesValue: {
             value: 0,
             get: function() {
@@ -85,8 +169,16 @@ export default can.Map.extend({
                 return 0;
             }
         },
+
+        /**
+         * @property {Object} graph.viewModel.xScale xScale
+         * @parent graph/viewModel
+         * @description D3 scale object representing the X scale of the graph.
+         * @option {Object} Defaults to `null`.
+         */
         xScale: {
             value: null,
+            type: '*',
             get: function() {
                 var domainEndValue = (this.attr('normalize')) ? 5 : this.attr('longestSeriesLength');
 
@@ -94,8 +186,16 @@ export default can.Map.extend({
                 return d3.scale.linear().domain([0, domainEndValue-1]).range([0, this.attr('width')]);
             }  
         },
+
+        /**
+         * @property {Object} graph.viewModel.yScale yScale
+         * @parent graph/viewModel
+         * @description D3 scale object representing the Y scale of the graph.
+         * @option {Object} Defaults to `null`.
+         */
         yScale: {
             value: null,
+            type: '*',
             get: function() {
                 var normalize = this.attr('normalize'),
                     domainBeginValue = normalize ? 0 : this.attr('lowestSeriesValue'),
@@ -106,7 +206,22 @@ export default can.Map.extend({
             }
         }
     },
+
+    /**
+     * @property {Array} graph.viewModel.series series
+     * @parent graph/viewModel
+     * @description Series that are part of the graph.
+     * @option {Array} Defaults to `[]`.
+     */
     series: [],
+
+    /**
+     * @function graph.viewModel.clearContainer clearContainer
+     * @parent graph/viewModel
+     * @description Clears a D3 container of all elements and executes a callback when completed.
+     * @param {Object} container D3 object who's elements are to be cleared.
+     * @param {Function} cb Callback to be run once all elements are cleared.
+     */
     clearContainer: function(container, cb) {
         // no clean way to remove all elements and receive a callback, so we have to make our own
         // http://stackoverflow.com/questions/23118779/d3-callback-function-after-remove
@@ -126,12 +241,26 @@ export default can.Map.extend({
             }).remove();
         }
     },
+
+    /**
+     * @function graph.viewModel.clearGraphLines clearGraphLines
+     * @parent graph/viewModel
+     * @description Clears the graph line container element and executes a callback when completed.
+     * @param {Function} cb Callback to be run once all graph lines are cleared.
+     */
     clearGraphLines: function(cb) {
         this.clearContainer(this.attr('lineContainerElement'), cb);
     },
+
+    /**
+     * @function graph.viewModel.refreshGraph refreshGraph
+     * @parent graph/viewModel
+     * @description Clears the graph lines and re-renders all lines.
+     */
     refreshGraph: function() {
         // TODO this method should just capture changes to any element on the graph and move into a define
         // TODO append all elements to a document fragment instead and update in the change event
+        // TODO only refresh lines that have changed, not all lines
 
         // bind to the serialized changes
         this.attr('seriesSerialized');
@@ -146,10 +275,26 @@ export default can.Map.extend({
             });
         });
     },
+
+    /**
+     * @function graph.viewModel.clearAxes clearAxes
+     * @parent graph/viewModel
+     * @description Clears the graph axes container element and executes a callback when completed.
+     * @param {Function} cb Callback to be run once all graph axes are cleared.
+     */
     clearAxes: function(cb) {
         this.clearContainer(this.attr('axisContainerElement'), cb);
     },
+
+    /**
+     * @function graph.viewModel.refreshAxes refreshAxes
+     * @parent graph/viewModel
+     * @description Clears the graph axes and re-renders all axes.
+     */
     refreshAxes: function() {
+        // TODO this method should just capture changes to the x/y scales or graph height/width and reload accordingly
+        // TODO split this up so that the X and Y axis can be refresh separately
+
         this.clearAxes(() => {
             var axisContainerElement = this.attr('axisContainerElement');
 
@@ -180,6 +325,13 @@ export default can.Map.extend({
                   .call(yAxisLeft);
         });
     },
+
+    /**
+     * @function graph.viewModel.renderBaseGraph renderBaseGraph
+     * @parent graph/viewModel
+     * @description Renders the base graph container and builds out structure of graph (axis and line container).
+     * @param {Object} graphBaseElement D3 object that will contain the graph structure.
+     */
     renderBaseGraph: function(graphBaseElement) {
         var margins = this.attr('margins');
         
@@ -197,11 +349,25 @@ export default can.Map.extend({
         this.refreshAxes();
         this.refreshGraph();
     },
+
+    /**
+     * @function graph.viewModel.addSeries addSeries
+     * @parent graph/viewModel
+     * @description Adds a series to the graph. This is called automatically when a `bit-series` subcomponent is inserted.
+     * @param {Object} series BitSeriesVM object.
+     */
     addSeries: function(series) {
         can.batch.start();
         this.attr('series').push(series);
         can.batch.stop();
     },
+
+    /**
+     * @function graph.viewModel.removeSeries removeSeries
+     * @parent graph/viewModel
+     * @description Removes a series from the graph. This is called automatically when a `bit-series` subcomponent is removed.
+     * @param {Object} series BitSeriesVM object.
+     */
     removeSeries: function(series) {
         var seriesList = this.attr('series');
         can.batch.start();
